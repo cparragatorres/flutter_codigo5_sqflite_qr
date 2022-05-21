8c946054703761bb7f41118cb0092aaaa44e974e
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-21 09:26:50 -0500

    Scanner Page 2

--- lib/pages/scanner_qr_page.dart
+++ lib/pages/scanner_qr_page.dart
@@ -1,11 +1,42 @@
 
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class ScannerQRPage extends StatelessWidget {
   const ScannerQRPage({Key? key}) : super(key: key);
 
+
+
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+        MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ,
+          ),
+        ],
+      ),
+    );
   }
 }
