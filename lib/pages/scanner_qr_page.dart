1aba5eaec84c2e4ddbc12a2f440a8b67149b9850
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-21 10:22:56 -0500

    Scanner Page 4

--- lib/pages/scanner_qr_page.dart
+++ lib/pages/scanner_qr_page.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -11,12 +10,9 @@ class ScannerQRPage extends StatefulWidget {
 }
 
 class _ScannerQRPageState extends State<ScannerQRPage> {
-
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
-
-
 
   @override
   void reassemble() {
@@ -29,18 +25,18 @@ class _ScannerQRPageState extends State<ScannerQRPage> {
 
   Widget _buildQrView(BuildContext context) {
     var scanArea = (MediaQuery.of(context).size.width < 400 ||
-        MediaQuery.of(context).size.height < 400)
-        ? 200.0
+            MediaQuery.of(context).size.height < 400)
+        ? 260.0
         : 300.0;
     return QRView(
       key: qrKey,
       onQRViewCreated: _onQRViewCreated,
       overlay: QrScannerOverlayShape(
-          borderColor: kBrandPrimaryColor,
-          borderRadius: 10,
-          borderLength: 30,
-          borderWidth: 7,
-          cutOutSize: scanArea,
+        borderColor: kBrandPrimaryColor,
+        borderRadius: 10,
+        borderLength: 30,
+        borderWidth: 7,
+        cutOutSize: scanArea,
       ),
       onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
     );
@@ -53,6 +49,7 @@ class _ScannerQRPageState extends State<ScannerQRPage> {
     controller.scannedDataStream.listen((scanData) {
       setState(() {
         result = scanData;
+        print(result!.code);
       });
     });
   }
@@ -77,7 +74,61 @@ class _ScannerQRPageState extends State<ScannerQRPage> {
       body: Column(
         children: [
           Expanded(
+            flex: 4,
             child: _buildQrView(context),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Column(
+                children: [
+                  Text(
+                    "Carnet escaneado: ",
+                    style: TextStyle(
+                      color: kFontPrimaryColor,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6.0,
+                  ),
+                  Text(
+                    "Por favor escanea un carnet",
+                    style: TextStyle(
+                      color: kFontPrimaryColor,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 52.0,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: kBrandPrimaryColor,
+                        // primary: Color(0xff04CD8B),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));
+                      },
+                      child: const Text(
+                        "Registrar carnet",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
           ),
         ],
       ),
