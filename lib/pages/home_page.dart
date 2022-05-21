47b46919cbb8632ccc1c3652fed784e7f90b1615
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-21 09:16:53 -0500

    Scanner Page 1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_sqflite_qr/pages/scanner_qr_page.dart';
 import 'package:flutter_codigo5_sqflite_qr/ui/general/colors.dart';
 import 'package:flutter_codigo5_sqflite_qr/ui/widgets/item_list_widget.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -59,7 +60,9 @@ class HomePage extends StatelessWidget {
               width: double.infinity,
               margin: const EdgeInsets.all(12.0),
               child: ElevatedButton.icon(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> ScannerQRPage()));
+                },
                 style: ElevatedButton.styleFrom(
                   primary: kBrandPrimaryColor,
                   shape: RoundedRectangleBorder(
---
+++ lib/pages/scanner_qr_page.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class ScannerQRPage extends StatelessWidget {
+  const ScannerQRPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
