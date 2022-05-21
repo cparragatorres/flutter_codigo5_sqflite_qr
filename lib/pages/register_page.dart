9df45e1485f36cef5f4f615499d2cc1eee8059b7
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-21 12:15:46 -0500

    Register Page 3

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_sqflite_qr/ui/general/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -31,10 +31,56 @@ class _RegisterPageState extends State<RegisterPage> {
           padding: const EdgeInsets.all(14.0),
           child: Column(
             children: [
-              Text(
+              const Text(
                 "Registrar nuevo carnet",
                 style: TextStyle(
                   fontSize: 16.0,
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                "Nombre completo:",
+                style: TextStyle(
+                  color: kFontPrimaryColor.withOpacity(0.6),
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.06),
+                        offset: const Offset(4, 4),
+                        blurRadius: 12.0),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Nombre completo",
+                    hintStyle: TextStyle(
+                      fontSize: 13.0,
+                      color: kFontPrimaryColor.withOpacity(0.4),
+                    ),
+                    prefixIcon: SvgPicture.asset(
+                      'assets/icons/bx-user.svg',
+                      height: 12.0,
+                      fit: BoxFit.scaleDown,
+                      color: kFontPrimaryColor.withOpacity(0.4),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
                 ),
               ),
             ],
