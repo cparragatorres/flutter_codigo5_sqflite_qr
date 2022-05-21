622ddb93d81555ebb9e96074085b322b56924518
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-21 12:08:08 -0500

    Register Page 2

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_sqflite_qr/ui/general/colors.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -11,6 +12,35 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: kFontPrimaryColor),
+        title: const Text(
+          "VacunApp Storage",
+          style: TextStyle(
+            color: kFontPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            children: [
+              Text(
+                "Registrar nuevo carnet",
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
